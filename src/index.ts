af9f0c6d036c1859bd8334119eb64c70f3d5f9b9
Kevin Valdovinos <119825966+KevinValdovinos@users.noreply.github.com>
2023-09-26 18:13:03 -0600

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import app from "config/express";
+const port=process.env.PORT||3000;
+const main=()=>{
+    try {
+        app.listen(port);
+        console.log('http://localhost:${port}');
+    } catch (error) {
+        
+    }
+}
+main();
